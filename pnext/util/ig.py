f46875ed24825b3e2228b7622b963cdf81a87e56
Pierre-Nick Durette <pndurette@gmail.com>
2014-01-18 00:49:14 -0500

    Smarter way to cache Instagram. Only rebuild when needed

--- pnext/util/ig.py
+++ pnext/util/ig.py
@@ -1,30 +1,45 @@
 from instagram.client import InstagramAPI
-import os, json, random
+import os, json, simplejson, random
 
 class MyInstagram:
-    MEDIA_IDS_FILE = os.path.join(os.path.dirname(__file__), "../data/instagrams.json")
+    # Ex.: { 'instagrams' : [{ 'media_id': '474834340988151412_10974184', 'img_url': '', 'link_url': '', 'title': ''}, ... ]}
+    DATA_FILE_LOC = os.path.join(os.path.dirname(__file__), "../data/instagrams.json")
     ACCESS_TOKEN = "10974184.1d5ac95.5478d52949e343b69a3d93997de90cd5"
     
     def __init__(self):
-        self.__media_ids_list = json.load(open(MyInstagram.MEDIA_IDS_FILE))['media_ids']
-        self.__media_ids_list = list(set(self.__media_ids_list)) # Quick'n' dirty dupe removal
-        self.__img_dicts = self.__get_img_dicts(self.__media_ids_list) 
+        self.__instagrams_list = []
+        self.__load_data()
+        self.__augment_data()
+        self.__save_data()
 
-    def __get_img_dicts(self, medias_ids_list):
+    def __load_data(self):
+        try:
+            self.__instagrams_list = json.load(open(MyInstagram.DATA_FILE_LOC))['instagrams']
+        except Exception, e:
+            print "Error loading file %s: %s" % (MyInstagram.DATA_FILE_LOC, str(e))
+
+    def __augment_data(self):
+        """ Populates any missing fields via api(media_id) """
         api = InstagramAPI(access_token=MyInstagram.ACCESS_TOKEN)
-        
-        img_dicts = []
-        for media_id in medias_ids_list:
-            print "Processing media '%s'" % media_id
-            media = api.media(media_id)
-            title = media.caption 
-            img = media.images['standard_resolution'].url
-            link = media.link
-            img_dicts.append({"title": title, "img": img, "link": link, "media_id": media_id})
-        return img_dicts
+        for i in self.__instagrams_list:
+            if not i['img_url'] or not i['link_url']:
+                # Only do actual api query if we are missing something (ex: new entries)
+                media = api.media(i['media_id'])
+                i['title'] = media.caption.text if media.caption else ""
+                i['img_url'] = media.images['standard_resolution'].url
+                i['link_url'] = media.link
+                print "Loaded new media: '%s' (title: '%s')" % (i['media_id'], i['title'])
+
+    def __save_data(self):
+        try:
+            print "Saving instagram file to: %s" % MyInstagram.DATA_FILE_LOC
+            with open(MyInstagram.DATA_FILE_LOC, "w") as f:
+                f.write(simplejson.dumps({"instagrams" : self.__instagrams_list}, indent=4))
+        except Exception, e:
+            print "Error saving file: %s" % str(e)
 
     def get_random_img(self):
-        return random.choice(self.__img_dicts)
+        return random.choice(self.__instagrams_list)
 
     def get_random_img_list_of_size(self, size):
-        return random.sample(self.__img_dicts, size)
+        return random.sample(self.__instagrams_list, size)

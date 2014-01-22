81aa7190f43bd9b092aa3923c040eef087312a9d
Pierre-Nick Durette <pndurette@gmail.com>
2014-01-21 21:10:19 -0500

    Took the viewport javascript sizing out  function Load it right away after jquery, to prevent this ugly moment when the viewport is not set until everything is loaded

--- pnext/static/js/main.js
+++ pnext/static/js/main.js
@@ -13,14 +13,14 @@
 // jQuery Main events
 $(window).load(function(){
     // Tweaking viewport size for device scaling
-    w = $(window).width();
-    if (w < 1025 && w > 1000) {
-        // iPad
-        $('meta[name=viewport]').attr('content','width=device-width, initial-scale=1 minimum-scale=1, maximum-scale=1');
-    } else if ($(window).width() <= 1000) { 
-        // iPhone
-        $('meta[name=viewport]').attr('content','width=device-width, initial-scale=0.65, minimum-scale=0.65, maximum-scale=0.65');
-    }
+    //w = $(window).width();
+    //if (w < 1025 && w > 1000) {
+    //    // iPad
+    //    $('meta[name=viewport]').attr('content','width=device-width, initial-scale=1 minimum-scale=1, maximum-scale=1');
+    //} else if ($(window).width() <= 1000) { 
+    //    // iPhone
+    //     $('meta[name=viewport]').attr('content','width=device-width, initial-scale=0.65, minimum-scale=0.65, maximum-scale=0.65');
+    //}
     //alert(w + " / " + $('meta[name=viewport]').attr('content'));
 
     // OnePage Scroll, tweaked

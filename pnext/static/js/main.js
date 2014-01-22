16736f209d7e80bdf02575bff2a4d7ce0753e869
Pierre-Nick Durette <pndurette@gmail.com>
2014-01-21 21:00:45 -0500

    Scaled back from 0.75 to 0.65 for smartphone

--- pnext/static/js/main.js
+++ pnext/static/js/main.js
@@ -19,7 +19,7 @@ $(window).load(function(){
         $('meta[name=viewport]').attr('content','width=device-width, initial-scale=1 minimum-scale=1, maximum-scale=1');
     } else if ($(window).width() <= 1000) { 
         // iPhone
-        $('meta[name=viewport]').attr('content','width=device-width, initial-scale=0.75, minimum-scale=0.75, maximum-scale=0.75');
+        $('meta[name=viewport]').attr('content','width=device-width, initial-scale=0.65, minimum-scale=0.65, maximum-scale=0.65');
     }
     //alert(w + " / " + $('meta[name=viewport]').attr('content'));
 
